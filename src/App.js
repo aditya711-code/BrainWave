b756c47976b5c607f8f96755a2818efe5632bb33
Aditya Mane <adityamane711@gmail.com>
2024-09-28 09:38:14 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import '../index.css';
+function App(){
+    return (
+      <div className='text-3xl font-bold underline'>
+        <h1>Hello React I am God</h1>
+      </div>
+    );
+}
+export default App;
